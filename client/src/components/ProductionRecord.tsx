d162cccb21b0f4362ddfe1e6ff6930ea1bd9c521
jakkitpoolsawat170119-dev <jakkitpoolsawat170119@gmail.com>
2026-07-17 17:53:24 +0700

    Update ProductionRecord.tsx

--- client/src/components/ProductionRecord.tsx
+++ client/src/components/ProductionRecord.tsx
@@ -213,7 +213,7 @@ const ProductionRecord: React.FC<ProductionRecordProps> = ({ operatorName, onHom
     "MLH 02", "Freshy Pineapple", "Operator Name", "Freshy Grape",
     "Freshy Punch", "Freshy blue Lemon", "Senorita Fres Mint","Senorita Strawberry",
     "Freshy Orange", "Signature Rose", "Freshy Shine Muscat Grape", "Freshy Peach",
-    "Freshy Mango", "Dilute W-Molass", "Freshy Brownsugar", "CIP",
+    "Freshy Mango", "Dilute W-Molass", "Freshy Brownsugar", "Freshy Blueberry", "CIP",
   ];
 
   const flavorColors: Record<string, { bg: string; border: string }> = {
